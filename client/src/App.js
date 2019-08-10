c8cb9d64c0dcae03307ebde678698f70253cb7fa
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-10 14:48:15 -0700

    apolloclient to connect client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {useQuery} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
 
 function App() {
+  const {loading, error, data} = useQuery(gql`
+    query {
+      posts {
+        id
+        title
+        content
+      }
+    }
+  `);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error!</div>;
+
   return (
     <div>
-      APP
+      <div>
+        {data.posts.map(({id, title, content}) => (
+          <div key={id}>
+            <h1>{title}</h1>
+            <p>{content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {ApolloProvider} from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
-const client = new ApolloProvider({
+const client = new ApolloClient({
   uri: 'http://localhost:4000/',
 });
 

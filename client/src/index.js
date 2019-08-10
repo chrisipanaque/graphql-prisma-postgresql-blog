d0fbd1890c9f4f711316d81555df5ac0a54f1daa
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-10 14:40:17 -0700

    ApolloProvider client

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {ApolloProvider} from '@apollo/react-hooks';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloProvider({
+  uri: 'http://localhost:4000/',
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
